3000ed88ff96ede4bdc03a0ad03a7ee6a8931d7b
cykoravish <ravishbisht85@gmail.com>
2026-01-31 06:49:44 +0530

    changes done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
-  return res.status(200).json({msg:"app running"});
+  return res.status(200).json({msg:"app running on v1"});
 });
 
 app.listen(PORT, ()=>{
